4bbd50dc9cb588e11934ed1d944bd7803bea061c
kamolazohidova <kzohidova567@gmail.com>
2022-08-13 20:22:35 +0500

    Add search button

--- js/app.js
+++ js/app.js
@@ -1,13 +1,21 @@
 const elMain = document.getElementById('main')
 const elInp = document.getElementById('input')
 const elBtn = document.getElementById('btn')
+const serachInput = document.getElementById('serachInput');
 
+let inputValue = '';
 
 let person = [
     { id: 1, name: "Robiya" },
     { id: 2, name: "Roziya" },
     { id: 3, name: "Ruqiya" },
 ]
+
+
+serachInput.addEventListener('change', (e)=> {
+    inputValue = e.target.value
+    getUser()
+})
 
 
 function delUser(id) {
@@ -17,14 +25,11 @@ function delUser(id) {
 
 
 function getUser() {
-    let user = ''
-    person.map((value) => {
-        user += `<div class="wrap">
+    elMain.innerHTML = person.map((value)=> (value.name + '').includes(inputValue) && (`<div class="wrap">
+        <h2 class="wrap__num">${value.id}</h2>
         <h2 class="wrap__title">${value.name}</h2>
         <button class="wrap__btn" onClick={delUser(${value.id})} >Delete</button>
-        </div>`
-    })
-    elMain.innerHTML = user
+    </div>`)).filter(Boolean).join('')
 }
 
 
@@ -35,7 +40,6 @@ function addUser(aliBuli) {
         aliBuli()
     }
 }
-
 
 elBtn.addEventListener('click', () => {
     console.log(elInp.value);
